dc70667160af3c68fcd43ffc7fba77150d66507d
Roland <roland@rolker.net>
2026-03-14 08:27:09 -0400

    Initialize Firebase and update script tag to module type

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGgppDv0m_0pw515CHM3NDZhvT_deFMYY",
+  authDomain: "daddy-camp.firebaseapp.com",
+  projectId: "daddy-camp",
+  storageBucket: "daddy-camp.firebasestorage.app",
+  messagingSenderId: "629092514614",
+  appId: "1:629092514614:web:653364644f2e3972fd1197",
+  measurementId: "G-6LW4GZV9EM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const state = {
     base: 0,
     face: 0,
